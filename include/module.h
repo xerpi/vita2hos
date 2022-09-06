8d3ba7719a9606c30ea54e13d3ec5a1cbef9506d
lotuspar <lotuspar0@gmail.com>
2022-09-07 00:30:23 +0800

    module: macros for module impl
    based on Vita3K module macros

--- include/module.h
+++ include/module.h
@@ -3,6 +3,11 @@
 
 #include <stdint.h>
 
+#define GET_EXPORT_NID(name) __nid_ ## name
+#define EXPORT(type, name, nid, ...) static const uint32_t GET_EXPORT_NID(name) = nid; type name(__VA_ARGS__)
+#define EXPORT_STUB EXPORT
+#define GET_EXPORT_ENTRY(name) {GET_EXPORT_NID(name), name}
+
 typedef struct {
 	uint32_t nid;
 	void *addr;
