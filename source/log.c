89dfdc3838a772e06d57c6fd30d25946e0f80e6a
TSRBerry <20988865+TSRBerry@users.noreply.github.com>
2022-10-19 03:26:13 +0200

    Add logging to file option (#31)
    * Add logging to file
    
    * Log current date and time at the beginning
    
    * Fix always logging the same time

--- source/log.c
+++ source/log.c
@@ -5,12 +5,34 @@
 
 bool log_to_fb_console;
 
+#ifdef VITA2HOS_LOG_TO_FILE
+const char log_file_path[] = "sdmc:/vita2hos/debug.log";
+
+void flog(const char * str)
+{
+	FILE *fp;
+
+	fp = fopen(log_file_path, "a");
+	if (!fp)
+		return;
+
+	fputs(str, fp);
+	fputc('\n', fp);
+
+	fclose(fp);
+}
+#endif
+
 void LOGSTR(const char *str)
 {
 	svcOutputDebugString(str, strlen(str));
 
 	if (log_to_fb_console)
 		puts(str);
+
+#ifdef VITA2HOS_LOG_TO_FILE
+	flog(str);
+#endif
 }
 
 void __attribute__((format(printf, 1, 2))) LOG(const char *fmt, ...)
@@ -27,4 +49,8 @@ void __attribute__((format(printf, 1, 2))) LOG(const char *fmt, ...)
 
 	if (log_to_fb_console)
 		puts(buf);
+
+#ifdef VITA2HOS_LOG_TO_FILE
+	flog(buf);
+#endif
 }
