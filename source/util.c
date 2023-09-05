156ee4e4d8f9105a462e44dd6b3b0c593e4cc38b
Sergi Granell <xerpi.g.12@gmail.com>
2023-09-05 13:59:28 +0200

    util_load_file: use libc functions

--- source/util.c
+++ source/util.c
@@ -1,60 +1,39 @@
+#include <errno.h>
 #include <stdlib.h>
-#include <switch.h>
 #include "log.h"
 #include "util.h"
 
 int util_load_file(const char *filename, void **data, uint32_t *size)
 {
-	FsFileSystem *fs;
-	FsFile file;
-	void *base;
-	s64 filesz;
-	u64 bytes_read;;
-	Result res;
+	FILE *fp;
 
-	fs = fsdevGetDeviceFileSystem("sdmc");
-	if (!fs) {
-		LOG("load file: error opening SD");
+	fp = fopen(filename, "rb");
+	if (!fp) {
+		LOG("util_load_file: could not open file \"%s\". Error: %s", filename, strerror(errno));
 		return -1;
 	}
 
-	res = fsFsOpenFile(fs, filename, FsOpenMode_Read, &file);
-	if (R_FAILED(res)) {
-		LOG("load file: error opening \"%s\": 0x%" PRIx32, filename, res);
+	fseek(fp, 0, SEEK_END);
+	*size = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+
+	*data = malloc(*size);
+	if (!*data) {
+		LOG("util_load_file: could not allocate memory");
+		fclose(fp);
 		return -1;
 	}
 
-	res = fsFileGetSize(&file, &filesz);
-	if (R_FAILED(res)) {
-		LOG("load file: could not get file size: 0x%" PRIx32, res);
-		goto err_close_file;
+	if (fread(*data, 1, *size, fp) != *size) {
+		LOG("util_load_file: could not read the file");
+		free(*data);
+		fclose(fp);
+		return -1;
 	}
 
-	base = malloc(filesz);
-	if (!base) {
-		LOG("load file: could not allocate memory");
-		goto err_close_file;
-	}
-
-	res = fsFileRead(&file, 0, base, filesz, FsReadOption_None, &bytes_read);
-	if (R_FAILED(res)) {
-		LOG("load file: could not read the file: 0x%" PRIx32, res);
-		goto err_free_data;
-	}
-
-	fsFileClose(&file);
-
-	*data = base;
-	*size = filesz;
+	fclose(fp);
 
 	return 0;
-
-err_free_data:
-	free(base);
-err_close_file:
-	fsFileClose(&file);
-
-	return -1;
 }
 
 int util_write_binary_file(const char *filename, const void *data, uint32_t size)
